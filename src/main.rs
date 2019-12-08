8491a540c9ca3037c11da7777502232d8d3e0c0b
Sjoerd <sjoerd-github@linuxonly.nl>
2019-12-08 14:22:24 +0100

    Somewhat working MSS parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+use std::env;
+
+extern crate pest;
+#[macro_use]
+extern crate pest_derive;
+
+use pest::Parser;
+
+#[derive(Parser)]
+#[grammar = "cartocss.pest"]
+struct CartoParser;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let p = CartoParser::parse(Rule::stylesheet, 
+    // "@steps-width-z14 + 2 * (@paths-background-width + @paths-bridge-casing-width)"
+    &contents 
+    ).unwrap_or_else(|e| panic!("{}", e));
+    println!("{:#?}", p);
 }
