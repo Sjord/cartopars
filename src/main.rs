95148e12a141ef91e617fd5347d6dd3e0e6c7a75
Sjoerd <sjoerd-github@linuxonly.nl>
2019-12-29 23:02:42 +0100

    Consume the node tree with pest_consume

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
+extern crate pest_consume;
 
 extern crate yaml_rust;
 use yaml_rust::YamlLoader;
@@ -12,13 +13,78 @@ use yaml_rust::YamlLoader;
 extern crate stopwatch;
 use stopwatch::Stopwatch;
 
-
-use pest::Parser;
+use pest_consume::Parser;
+use pest_consume::Error;
+use pest_consume::match_nodes;
 use pest::iterators::Pairs;
+
+type Node<'i> = pest_consume::Node<'i, Rule, ()>;
+type Result<T> = std::result::Result<T, Error<Rule>>;
+
+type Ruleset = String;
 
 #[derive(Parser)]
 #[grammar = "cartocss.pest"]
 struct CartoParser;
+
+#[derive(Debug)]
+enum Statement {
+    Assignment(Assignment),
+    Ruleset(Ruleset)
+}
+
+#[derive(Debug)]
+struct Assignment {
+    key: String,
+    value: String,
+}
+
+struct Stylesheet {
+    assignments: Vec<Assignment>,
+    rulesets: Vec<Ruleset>
+}
+
+#[pest_consume::parser]
+impl CartoParser {
+    fn EOI(_input: Node) -> Result<()> {
+        Ok(())
+    }
+
+    fn color_keyword(input: Node) -> Result<&str> {
+        Ok(input.as_str())
+    }
+
+    fn variable(input: Node) -> Result<String> {
+        Ok(input.as_str().to_owned())
+    }
+
+    fn values(input: Node) -> Result<String> {
+        Ok(input.as_str().to_owned())
+    }
+
+    fn assignment(input: Node) -> Result<Assignment> {
+        Ok(match_nodes!(input.into_children();
+            [variable(k), values(v)] => Assignment { key: k, value: v }
+        ))
+    }
+
+    fn ruleset(input: Node) -> Result<Ruleset> {
+        Ok(input.as_str().to_owned())
+    }
+
+    fn statement(input: Node) -> Result<Statement> {
+        Ok(match_nodes!(input.into_children();
+            [assignment(a)] => Statement::Assignment(a),
+            [ruleset(r)] => Statement::Ruleset(r),
+        ))
+    }
+
+    fn stylesheet(input: Node) -> Result<Vec<Statement>> {
+        Ok(match_nodes!(input.into_children();
+            [statement(s).., EOI(_)] => s.collect(),
+        ))
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -31,11 +97,13 @@ fn main() {
         let path = mml_path.with_file_name(ss.as_str().unwrap());
         let contents = fs::read_to_string(&path).unwrap();
         sw.start();
-        let ast = CartoParser::parse(Rule::stylesheet, &contents)
+        let node = CartoParser::parse(Rule::stylesheet, &contents)
             .map_err(|e| e.with_path(path.to_str().unwrap()))
-            .unwrap();
+            .unwrap().single().unwrap();
+        let ast = CartoParser::stylesheet(node);
         sw.stop();
-        println!("{} {}", ss.as_str().unwrap(), sw.elapsed_ms());
+        // println!("{} {}", ss.as_str().unwrap(), sw.elapsed_ms());
+        println!("{:#?}", ast);
         sw.reset();
     }
 }
